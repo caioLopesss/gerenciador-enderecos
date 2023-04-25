165912a77c125021191d7137f97488555bf39686
Caio Lopes <caiolopeis95@gmail.com>
2023-04-25 15:22:50 -0300

    criando estrutura inicial react

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,13 @@
+import './header.css';
+
+function Header() {
+    return (
+        <header>
+            <a href="#">Usúarios</a>
+            <a href="#">Cadastrar usúario</a>
+            <a href="#">Cadastrar endereço</a>
+        </header>
+    );
+};
+
+export default Header
